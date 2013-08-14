3f4f7856836a43ca91ee7e20826319091ea1ab42
Top Cat <webmaster@thomasc.co.uk>
2013-08-14 12:41:40 +0100

    An error document seems clever...

---
+++ error.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+	<title>Error <?php print $_GET['e']; ?> | Mirror</title>
+	<link rel="shortcut icon" href="https://cdn.spout.org/img/ico/spout.ico" type="image/x-icon"> 
+	<link rel="stylesheet" href="https://cdn.spout.org/css/error.css" type="text/css">
+</head>
+<body>
+	<a href="" border="none" alt="Spout"><img id="logo" src="https://cdn.spout.org/img/icon/spout_error.png" width="500px" /></a>
+	Error <?php print $_GET['e']; ?><br />
+	<span id="msg2">Check the url or try again later...</span><br />
+</body>
+</html>
